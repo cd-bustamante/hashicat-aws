aa316fa1cb1a90f0e3fc319a92c9465b30ceba73
Cesar David <cesar.bustamante@slalom.com>
2021-06-16 18:32:16 +0000

    removing code variables

--- variables.tf
+++ variables.tf
@@ -5,9 +5,9 @@
 # Terraform code. If you create a variable with no default, the user will be
 # prompted to enter it (or define it via config file or command line flags.)
 
-variable "prefix" {
-  description = "This prefix will be included in the name of most resources."
-}
+#variable "prefix" {
+#  description = "This prefix will be included in the name of most resources."
+#}
 
 variable "region" {
   description = "The region where the resources are created."
@@ -34,17 +34,17 @@ variable "admin_username" {
   default     = "hashicorp"
 }
 
-variable "height" {
-  default     = "400"
-  description = "Image height in pixels."
-}
+#variable "height" {
+#  default     = "400"
+#  description = "Image height in pixels."
+#}
 
-variable "width" {
-  default     = "600"
-  description = "Image width in pixels."
-}
+#variable "width" {
+#  default     = "600"
+#  description = "Image width in pixels."
+#}
 
-variable "placeholder" {
-  default     = "placekitten.com"
-  description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
-}
+#variable "placeholder" {
+#  default     = "placekitten.com"
+#  description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
+#}
