be8a14bb0c28665efe638309dff695ccec2b13c0
Cesar David <cesar.bustamante@slalom.com>
2021-06-16 18:45:57 +0000

    adding variables

--- variables.tf
+++ variables.tf
@@ -34,17 +34,17 @@ variable "admin_username" {
   default     = "hashicorp"
 }
 
-#variable "height" {
-#  default     = "400"
-#  description = "Image height in pixels."
-#}
+variable "height" {
+  default     = "400"
+  description = "Image height in pixels."
+}
 
-#variable "width" {
-#  default     = "600"
-#  description = "Image width in pixels."
-#}
+variable "width" {
+  default     = "600"
+  description = "Image width in pixels."
+}
 
-#variable "placeholder" {
-#  default     = "placekitten.com"
-#  description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
-#}
+variable "placeholder" {
+  default     = "placekitten.com"
+  description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
+}
